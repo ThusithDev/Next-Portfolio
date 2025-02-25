911fca6364e0d6bae7c058318559b33cc08289ee
ThusithDev <anjulathusith@gmail.com>
2025-02-25 11:07:11 +0530

    Header and Footer

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '@/components/header'
+
+export default function Home() {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="text-3x1 font-bold">Portfolio</h1>
+      </div>
+    </section>
+  )
+}
+
